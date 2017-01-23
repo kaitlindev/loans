063eee609c35d90612fc246b035ca07044285966
Kaitlin Kidd <kkiddography@gmail.com>
2017-01-23 16:01:11 -0500

    Finished two different views: “monthly payment” and “payoff time”.

--- js/loanApp.js
+++ js/loanApp.js
@@ -1,18 +1,21 @@
 var loanApp = angular.module('loanApp', ['ngMessages', '720kb.tooltips', 'ngRoute']);
   loanApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-
     when('/monthlyPayment', {
        templateUrl: 'monthlyPayment.htm',
        controller: 'monthlyPaymentController'
     }).
-
     when('/payoffTime', {
        templateUrl: 'payoffTime.htm',
        controller: 'payoffTimeController'
     }).
-
     otherwise({
        redirectTo: '/monthlyPayment'
     });
   }]);
+  /* Round up value to closest whole number */
+  loanApp.filter('roundup', function(){
+  return function(input) {
+    return Math.ceil(+input);
+  };
+})
--- js/payoffTimeController.js
+++ js/payoffTimeController.js
@@ -1,3 +1,29 @@
 loanApp.controller('payoffTimeController', function($scope) {
-   $scope.message = "Test";
+  $scope.loan = {
+   principal: 1000,
+   interestRate: 5,
+   payment: 100,
+   principalToolTip: "Total sum of money lent or invested.",
+   interestRateToolTip: "Amount charged by a lender expressed as a % of the principal.",
+   paymentToolTip: "Monthly payment made on loan total.",
+   interestPrnct: function() { /* convert to % */
+      var loan = $scope.loan;
+      var interest = loan.interestRate;
+      var interestPrnct = (interest  / 100);
+      return interestPrnct;
+   },
+   payoffMonths: function() {
+      var loan = $scope.loan;
+      var m = loan.payment;
+      var p = loan.principal;
+      var r = loan.interestPrnct();
+      var payoffMonths = ((Math.log10(m) - Math.log10(m - ((p * r)/12))) / Math.log10(1 + (r /12)));
+      return payoffMonths;
+   },
+   payoffYears: function() {
+      var loan = $scope.loan;
+      var payoffYears = loan.payoffMonths() / 12;
+      return payoffYears;
+   }
+  };
 });
